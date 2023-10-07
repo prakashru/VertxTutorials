e0ccf493f8628a1c106c27adc3172a6609ff7431
emunityind <prakash@emunity.in>
2023-10-07 20:59:39 +0800

    Added Future Promise Test methods
    Added Vertx Study

---
+++ vertxstarter/src/test/java/com/vertx/study/vertxstarter/PromiseFutureOperationTest.java
@@ -0,0 +1,54 @@
+package com.vertx.study.vertxstarter;
+
+import io.vertx.core.Future;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(VertxExtension.class)
+public class PromiseFutureOperationTest {
+
+  @Test
+  void promise_success(Vertx vertx, VertxTestContext context){
+    final Promise<String> promise = Promise.promise();
+    System.out.println("Started Promise");
+    vertx.setTimer(500, id->{
+      System.out.println("Receiving Promise ");
+      promise.complete("Success!");
+      context.completeNow();
+    });
+
+    System.out.println("Finished");
+  }
+  @Test
+  void promise_failure(Vertx vertx, VertxTestContext context){
+    final Promise<String> promise = Promise.promise();
+    System.out.println("Started Promise");
+    vertx.setTimer(500, id->{
+      System.out.println("Failing Promise ");
+      promise.fail(new RuntimeException("Failed!"));
+      System.out.println("Failed...");
+      context.completeNow();
+    });
+    System.out.println("Finished");
+  }
+
+  @Test
+  void future_success(Vertx vertx, VertxTestContext context) {
+    final Promise<String> promise= Promise.promise();
+    System.out.println("Future started");
+    vertx.setTimer(500, id->{
+      promise.complete("Complete the Operation");
+      System.out.println("Promise done");
+      context.completeNow();
+    });
+   final Future<String> future=promise.future();
+   future.onSuccess(result->{
+     System.out.println("End " +result);
+     context.completeNow();
+   }).onFailure(context::failNow);
+  }
+}
