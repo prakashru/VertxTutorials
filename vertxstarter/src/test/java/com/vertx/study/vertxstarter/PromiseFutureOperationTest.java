09ff5ddf53b019cd0e1281f05c1c1c4004f90c51
emunityind <prakash@emunity.in>
2023-10-08 07:30:52 +0800

    Update PromiseFutureOperationTest.java

--- vertxstarter/src/test/java/com/vertx/study/vertxstarter/PromiseFutureOperationTest.java
+++ vertxstarter/src/test/java/com/vertx/study/vertxstarter/PromiseFutureOperationTest.java
@@ -1,12 +1,17 @@
 package com.vertx.study.vertxstarter;
 
+import io.vertx.core.CompositeFuture;
 import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.util.stream.BaseStream;
 
 @ExtendWith(VertxExtension.class)
 public class PromiseFutureOperationTest {
@@ -68,5 +73,104 @@ public class PromiseFutureOperationTest {
         context.completeNow();
       });
   }
+
+
+  @Test
+  void future_map(Vertx vertx, VertxTestContext context) {
+    final Promise<String> promise = Promise.promise();
+    System.out.println("Started Promise");
+    vertx.setTimer(500, id->{
+      System.out.println("Receiving Promise ");
+      promise.complete("Success!");
+      context.completeNow();
+    });
+    final Future<String> future=promise.future();
+    future
+      .map(asString->{
+        System.out.println("Map String to JsonObject");
+        return new JsonObject().put("key","Json Object!!!");
+      })
+        .map(jsonObject->new JsonArray().add(jsonObject))
+        .onSuccess(result->{
+            System.out.println("Result {} "+result+" Type "+result.getClass().getName());
+            context.completeNow();
+          })
+        .onFailure(context::failNow);
+
+    System.out.println("Finished");
+
+  }
+
+  @Test
+  void future_coordination(Vertx vertx, VertxTestContext context) {
+    vertx.createHttpServer()
+      .requestHandler(request-> System.out.println(request))
+      .listen(8080)
+      .onFailure(context::failNow)
+      .onSuccess(server->{
+        System.out.println("Server Started on "+server.actualPort());
+        context.completeNow();
+      });
+  }
+
+  @Test
+  void future_coordinationwithFuture(Vertx vertx, VertxTestContext context) {
+
+    vertx.createHttpServer()
+      .requestHandler(request-> System.out.println(request))
+      .listen(10000)
+      .compose(server->{
+        System.out.println("Pass more info");
+        return Future.succeededFuture(server);
+      })
+      .compose(server1->{
+        System.out.println("even more info");
+        return Future.succeededFuture(server1);
+      })
+      .onFailure(context::failNow)
+      .onSuccess(server2->{
+        System.out.println("Server Started on "+server2.actualPort());
+        context.completeNow();
+      });
+  }
+
+
+  @Test
+  void future_composite(Vertx vertx, VertxTestContext context) {
+    var one = Promise.<Void>promise();
+    var two = Promise.<Void>promise();
+    var three = Promise.<Void>promise();
+
+    var futureOne = one.future();
+    var futuretwo = two.future();
+    var futurethree = three.future();
+
+    CompositeFuture.all(futureOne, futuretwo, futurethree)
+      .compose(oneserver->{
+        System.out.println("Passing first info");
+        return Future.succeededFuture(oneserver);
+      })
+      .onFailure(error->{
+        System.out.println("Error: "+error);
+        context.completeNow();
+      })
+      .onSuccess(result->{
+        System.out.println("Result {} "+ result);
+        context.completeNow();
+      });
+
+    vertx.setTimer(500, id->{
+      System.out.println("One starting!!!");
+      one.complete();
+      System.out.println("two starting!!!");
+      two.fail("Two failed");
+      System.out.println("three starting!!!");
+      //three.fail("Failed");
+      three.complete();
+    });
+//https://betterprogramming.pub/understanding-vert-x-compose-vs-map-d464dee78980
+
+  }
+
 
 }
