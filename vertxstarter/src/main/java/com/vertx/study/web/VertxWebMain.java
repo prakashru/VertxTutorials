df62b583b8b358dc2dc999da1e6740812fe4eb4a
emunityind <prakash@emunity.in>
2023-10-10 02:57:57 +0800

    localhost:8888/assets

--- vertxstarter/src/main/java/com/vertx/study/web/VertxWebMain.java
+++ vertxstarter/src/main/java/com/vertx/study/web/VertxWebMain.java
@@ -5,7 +5,9 @@ import io.vertx.core.AbstractVerticle;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
 
 import java.util.UUID;
 
@@ -27,31 +29,48 @@ public class VertxWebMain extends AbstractVerticle {
 
   }
 
+
   @Override
   public void start(final Promise<Void> startPromise) throws Exception {
     System.out.println("Started Vertx Web");
-//    vertx.deployVerticle(VertxWebMain.class.getName(),
-//      new DeploymentOptions()
-//        .setInstances(4)
-//        .setConfig(new JsonObject()
-//          .put("id", UUID.randomUUID().toString())
-//          .put("name", VerticleN.class.getSimpleName())
-//        )
-//    );
-//    startPromise.complete();
-    vertx.createHttpServer().requestHandler(req->{
-      req.response()
-        .putHeader("context-type", "text/plain")
-        .end("Hello World!");
-    }).listen(8888, http->{
-      if(http.succeeded()){
-        startPromise.complete();
-        System.out.println("HTTP server started on port 8888");
-      } else {
-        System.out.println("HTTP server failed to start on port 8888");
-        startPromise.fail(http.cause());
-      }
-    });
+    final Router routerApi= Router.router(vertx);
+   routerApi.get("/assets").handler(context->{
+     final JsonArray response=new JsonArray();
+     response.add(new JsonObject().put("symbol", "AAPL"))
+       .add(new JsonObject().put("symbol","AMZN"))
+       .add(new JsonObject().put("symbol", "NFLX"));
+     System.out.println("Response {} "+response);
+     System.out.println("Response {} "+context.normalizedPath());
+     context.response().end(response.toBuffer());
+   });
+
+//    vertx.createHttpServer()
+//      .requestHandler(req->{
+//      req.response()
+//        .putHeader("context-type", "text/plain")
+//        .end("Hello World!");
+//    }).listen(8888, http->{
+//      if(http.succeeded()){
+//        startPromise.complete();
+//        System.out.println("HTTP server started on port 8888");
+//      } else {
+//        System.out.println("HTTP server failed to start on port 8888");
+//        startPromise.fail(http.cause());
+//      }
+//    });
+    vertx.createHttpServer()
+      .requestHandler(routerApi)
+      .exceptionHandler(error->{
+        System.out.println("Error "+ error);
+      }).listen(8888 , http->{
+        if (http.succeeded()) {
+          startPromise.complete();
+          System.out.println("Started at port 8888");
+        } else {
+          startPromise.fail(http.cause());
+          System.out.println("Failed");
+        }
+      });
 
   }
 }
